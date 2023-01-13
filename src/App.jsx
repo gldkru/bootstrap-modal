a00ec841307f3d7d39e5d04eb0dae77572beb4f2
Kirill Gladkikh <onest.kill@yandex.ru>
2023-01-13 21:14:53 +0400

    feat(react): Modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Container from './components/Container'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Button from './components/Button'
+import Modal from './components/Modal'
 
 export function App() {
   return (
     <Container>
       <Header />
       <Button data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</Button>
+      <Modal id="exampleModal" />
       <Footer />
     </Container>
   )
