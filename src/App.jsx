5be0c8bf32e88896128516273fc61ce91a5e94c7
Kirill Gladkikh <onest.kill@yandex.ru>
2023-01-13 21:33:04 +0400

    feat(react): Form

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Button from './components/Button'
 import Modal from './components/Modal'
+import Form from './components/Form'
 
 export function App() {
   return (
     <Container>
       <Header />
       <Button data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</Button>
-      <Modal id="exampleModal" />
+      <Modal id="exampleModal">
+        <Form />
+      </Modal>
       <Footer />
     </Container>
   )
