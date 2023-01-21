7fc73625a4e608792cedff9de638b31dff020554
Kirill Gladkikh <onest.kill@yandex.ru>
2023-01-21 17:05:57 +0400

    feat(react): Modal, js bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Container from './components/Container'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Button from './components/Button'
+import Modal from './components/Modal'
 
 export function App() {
   return (
     <Container>
       <Header />
       <Button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</Button>
+      <Modal id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true"></Modal>
       <Footer />
     </Container>
   )
