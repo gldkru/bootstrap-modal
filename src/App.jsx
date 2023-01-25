3840a8dc576f27c74009bc227627521af75fd924
Kirill Gladkikh <onest.kill@yandex.ru>
2023-01-25 22:03:45 +0400

    feat(react): Form, useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Button from './components/Button'
 import Modal from './components/Modal'
+import Form from './components/Form'
 
 export function App() {
   return (
     <Container>
       <Header />
-      <Button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</Button>
-      <Modal id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true"></Modal>
+      <Button type='button' data-bs-toggle='modal' data-bs-target='#exampleModal'>
+        Launch demo modal
+      </Button>
+      <Modal id='exampleModal' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+        <Form />
+      </Modal>
       <Footer />
     </Container>
   )
